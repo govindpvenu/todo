06bbcdb2ef349b7576ce82dc8d2c86ec0c235456
govindpvenu <govindpvenu.txt@gmail.com>
2025-04-23 14:09:29 +0530

    style: wisteria font setup

---
+++ constants/fonts.ts
@@ -0,0 +1,22 @@
+import { Inter, Poppins } from "next/font/google";
+import localFont from "next/font/local";
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: "300",
+  variable: "--font-poppins",
+});
+
+export const Wisteria = localFont({
+  weight: "500",
+  variable: "--font-wisteria",
+  display: "swap",
+  style: "normal",
+  src: "../public/Wisteria.woff",
+});
+//Back up font
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
